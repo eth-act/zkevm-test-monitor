38885822d5311fb93c8622cb1aadbcd81b77dba5
Cody Gunton <codygunton@gmail.com>
2025-10-30 13:31:58 -0400

    Enhance comments for makefile execution clarity
    Added comments to clarify the makefile execution process.

--- riscof/plugins/openvm/riscof_openvm.py
+++ riscof/plugins/openvm/riscof_openvm.py
@@ -173,6 +173,8 @@ class openvm(pluginTemplate):
       # following line. Note this will prevent any signature checking or report generation.
       #raise SystemExit
 
+      # once the make-targets are done and the makefile has been created, run all the targets in
+      # parallel using the make command set above.
       make.execute_all(self.work_dir)
 
       # if target runs are not required then we simply exit as this point after running all
