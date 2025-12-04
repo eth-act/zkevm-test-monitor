5ca25197fd52ee737540e1c3d0fa48bd24da4536
Cody Gunton <codygunton@gmail.com>
2025-12-03 21:39:12 -0500

    more timeout oof

--- riscof/plugins/zisk/riscof_zisk.py
+++ riscof/plugins/zisk/riscof_zisk.py
@@ -188,7 +188,7 @@ class zisk(pluginTemplate):
 
       # once the make-targets are done and the makefile has been created, run all the targets in
       # parallel using the make command set above.
-      make.execute_all(self.work_dir, timeout=3600)
+      make.execute_all(self.work_dir, timeout=7200)
 
       # if target runs are not required then we simply exit as this point after running all
       # the makefile targets.
